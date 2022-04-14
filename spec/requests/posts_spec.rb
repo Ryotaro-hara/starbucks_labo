56a80d9e0f1e7d048d903267781adf7855635dbf
原遼太朗 <ryotarohara@hararyoutarounoMacBook-Pro.local>
2022-04-14 14:06:03 +0900

    Add: Rspecリクエストテスト追加

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,10 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe "Posts", type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get "/"
-      expect(response).to have_http_status(:success)
+
+  describe "GET #index" do
+
+    before do
+      get root_path
+    end
+
+    it "レスポンスが正しく返ってくる事" do
+      expect(response).to have_http_status(200)
     end
   end
 
