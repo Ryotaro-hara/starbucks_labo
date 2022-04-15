eb386c5d6a0fbaafb28610e77a0135e2d2982017
原遼太朗 <ryotarohara@hararyoutarounoMacBook-Pro.local>
2022-04-15 21:42:52 +0900

    Update: エラー文日本語表示の変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 20 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
   validates :email, length: { maximum: 50 },
-                    format: { with: VALID_EMAIL_REGEX },
+                    format: { with: VALID_EMAIL_REGEX, message: 'が有効ではありません' },
                     uniqueness: { case_sensitive: false }
          
 end
