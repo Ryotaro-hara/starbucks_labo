ad16577d9d5d376eb654acca2f016e9031e7b96a
原遼太朗 <ryotarohara@hararyoutarounoMacBook-Pro.local>
2022-04-21 11:18:08 +0900

    Update: ストロングパラメーターの追加・設定の追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,8 @@ class User < ApplicationRecord
                     uniqueness: { case_sensitive: false }
   
   def update_without_current_password(params, *options)
+    params.delete(:current_password)
+    
     if params[:password].blank? && params[:password_confirmation].blank?
       params.delete(:password)
       params.delete(:password_confirmation)
