05b45b18609dc0ea847705ae9674a76725484575
原遼太朗 <ryotarohara@hararyoutarounoMacBook-Pro.local>
2022-04-24 18:33:50 +0900

    Add: リクエストテスト用ログインモジュールの追加

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -64,4 +64,5 @@ RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
   config.include Devise::Test::IntegrationHelpers, type: :request
   config.include LoginModule
+  config.include LoginRequestModule
 end
---
+++ spec/support/login_request_module.rb
@@ -0,0 +1,5 @@
+module LoginRequestModule
+  def sign_in_as(user)
+    post new_user_session_path, params: { session: { email: user.email, password: user.password } }
+  end
+end
