690b717e0adfc2e0eb0b2d45321feec152f177b8
原遼太朗 <ryotarohara@hararyoutarounoMacBook-Pro.local>
2022-04-24 18:36:41 +0900

    Add: request specテストの追加

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -3,13 +3,36 @@ require 'rails_helper'
 RSpec.describe "Users", type: :request do
   let!(:user) { create(:user) }
 
-  it "新規登録ページへのレスポンスが正しく返ってくる事" do
-    get new_user_registration_path
-    expect(response).to have_http_status(200)
+  describe "ログイン前" do
+    it "新規登録ページへのレスポンスが正しく返ってくる事" do
+      get new_user_registration_path
+      expect(response).to have_http_status(200)
+    end
+
+    it "ログインページへのレスポンスが正しく返ってくる事" do
+      get new_user_session_path
+      expect(response).to have_http_status(200)
+    end
   end
 
-  it "ログインページへのレスポンスが正しく返ってくる事" do
-    get new_user_session_path
-    expect(response).to have_http_status(200)
+  describe "ログイン後" do
+    before do
+      sign_in user
+    end
+
+    it "ユーザー詳細ページへのレスポンスが正常に返ってくる事" do
+      get user_path(user)
+      expect(response).to have_http_status(200)
+    end
+
+    it "ユーザー編集ページへのレスポンスが正常に返ってくる事" do
+      get edit_user_path(user)
+      expect(response).to have_http_status(200)
+    end
+
+    it "ユーザーパスワード編集ページへのレスポンスが正常に返ってくる事" do
+      get edit_user_registration_path
+      expect(response).to have_http_status(200)
+    end
   end
 end
