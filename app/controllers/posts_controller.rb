b9e03f44b4c17ce55c1563fa6934c39af9418306
原遼太朗 <ryotarohara@hararyoutarounoMacBook-Pro.local>
2022-04-28 21:14:58 +0900

    Add: 新規投稿ベージのコントローラーとルーティングの設定

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,24 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to root_path, notice: "新規投稿に成功しました"
+    else
+      flash.now[:alert] = "新規投稿に失敗しました"
+      render "new"
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content, :extra_fee, :change, :image)
+  end
 end
