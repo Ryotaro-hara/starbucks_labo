e97a3da7f5473098b3ef8a2df017ec125d054337
原遼太朗 <ryotarohara@hararyoutarounoMacBook-Pro.local>
2022-04-29 10:39:29 +0900

    Add: バリデーションの追加・エラー内容の日本語化

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+  enum change: { ちょい変: 1, 激変: 2 }
+  
+  with_options presence: true do
+    validates :title
+    validates :content
+    validates :extra_fee
+    validates :change
+    validates :image
+  end
+  
+  mount_uploader :image, ImageUploader
 end
