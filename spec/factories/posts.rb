ef275e97760cdd04c5555d4b7dfcb38e1e7a3f41
原遼太朗 <ryotarohara@hararyoutarounoMacBook-Pro.local>
2022-04-30 03:41:17 +0900

    Add: テストデータ・テスト画像の追加

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,7 +1,14 @@
 FactoryBot.define do
   factory :post do
-    title { "MyString" }
-    content { "MyText" }
+    title { "テスト投稿" }
+    content { "テスト用投稿内容" }
     extra_fee { 1 }
+    change { "ちょい変" }
+    image { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/factories/post.test.png'))}
+    association :user
+
+    trait :invalid do
+      title { nil }
+    end
   end
 end
