da46271e917c8b28ce64aae534f087fb56204a1e
原遼太朗 <ryotarohara@hararyoutarounoMacBook-Pro.local>
2022-05-03 14:35:37 +0900

    Add: editアクションとupdateアクションをpostsコントローラーに追加・編集可能ユーザーの設定

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, except: :index
+  before_action :set_target_post, only: [:show, :edit, :update]
+  before_action :ensure_correct_user, only: [:edit, :update]
 
   def index
     @posts = Post.all
@@ -20,12 +22,34 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to post_path(@post.id), notice: "投稿を更新しました"
+    else
+      flash.now[:alert] = "投稿の編集に失敗しました"
+      render "edit"
+    end
   end
 
   private
 
   def post_params
     params.require(:post).permit(:title, :content, :extra_fee, :change, :image).merge(user_id: current_user.id)
+  end
+
+  def set_target_post
+    @post = Post.find(params[:id])
+  end
+
+  def ensure_correct_user
+    @post = Post.find(params[:id])
+    if @post.user_id != current_user.id
+      redirect_to root_path, alert: "他のユーザーの投稿は編集できません"
+    end
   end
 end
