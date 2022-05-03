421ed9721fda6205d1c54055296734f5c6eaa604
原遼太朗 <ryotarohara@hararyoutarounoMacBook-Pro.local>
2022-05-04 00:33:45 +0900

    Add: 投稿編集ページrequestスペックテストの追加

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -72,4 +72,63 @@ RSpec.describe "Posts", type: :request do
       expect(response).not_to have_http_status(200)
     end
   end
+
+  describe "GET #edit" do
+    before do
+      sign_in user
+      get edit_post_path(new_post)
+    end
+
+    it "レスポンスが正しく返ってくる事" do
+      expect(response).to have_http_status(200)
+    end
+
+    it "編集前のデータが表示されている事" do
+      expect(response.body).to include new_post.title
+      expect(response.body).to include new_post.content
+      expect(response.body).to include new_post.extra_fee.to_s
+      expect(response.body).to include new_post.change
+    end
+  end
+
+  describe "PATCH #update" do
+    before do
+      sign_in user
+    end
+
+    context "パラメーターが正常な場合" do
+      let!(:post_params) { attributes_for(:post, title: "other") }
+
+      it "リクエストが成功する事" do
+        patch post_path(new_post), params: { post: post_params }
+        expect(response).to have_http_status(302)
+      end
+
+      it "titleが更新される事" do
+        expect { 
+          patch post_path(new_post), params: { post: post_params }
+        }.to change { new_post.reload.title }.from("テスト投稿").to("other")
+      end
+    end
+
+    context "パラメーターが異常な場合" do
+      let(:invalid_post_params) { attributes_for(:post, :invalid) }
+      
+      it "リクエストが成功する事" do
+        patch post_path(new_post), params: { post: invalid_post_params }
+        expect(response).to have_http_status(200)  
+      end
+
+      it "titleが更新されない事" do
+        expect { 
+          patch post_path(new_post), params: { post: invalid_post_params }
+        }.not_to change(new_post.reload, :title)
+      end
+
+      it "エラーが表示される事" do
+        patch post_path(new_post), params: { post: invalid_post_params }
+        expect(response.body).to include "投稿の編集に失敗しました"  
+      end
+    end
+  end
 end
