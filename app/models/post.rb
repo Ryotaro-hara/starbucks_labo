27627dda6d2b1db9344dc06887246b122de51992
原遼太朗 <ryotarohara@hararyoutarounoMacBook-Pro.local>
2022-05-09 09:06:49 +0900

    Add: バリデーション・アソシエーション・いいね状態を判定するメソッドの追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
   
   enum change: { ちょい変: 1, 激変: 2 }
   
@@ -10,6 +11,10 @@ class Post < ApplicationRecord
     validates :extra_fee
     validates :change
     validates :image
+  end
+
+  def favorite?(user)
+    favorites.where(user_id: user.id).exists?
   end
   
   mount_uploader :image, ImageUploader
