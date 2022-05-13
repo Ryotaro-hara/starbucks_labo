540405fb95059ecb4426ed0e5c17a831223de5a1
原遼太朗 <ryotarohara@hararyoutarounoMacBook-Pro.local>
2022-05-13 17:54:36 +0900

    Add: テストデータの追加

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :post do
     title { "テスト投稿" }
     content { "テスト用投稿内容" }
+    drink_type { "期間限定ドリンク" }
     extra_fee { 1 }
     change { "ちょい変" }
     image { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/factories/post.test.png'))}
