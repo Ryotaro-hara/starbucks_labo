66e78f61cffa789cd41931487d2419248341aba9
原遼太朗 <ryotarohara@hararyoutarounoMacBook-Pro.local>
2022-05-13 11:04:47 +0900

    Add: 新規投稿のストロングパラメーターにdrink_typeを追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -49,7 +49,7 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :content, :extra_fee, :change, :image).merge(user_id: current_user.id)
+    params.require(:post).permit(:title, :content, :drink_type, :extra_fee, :change, :image).merge(user_id: current_user.id)
   end
 
   def set_target_post
